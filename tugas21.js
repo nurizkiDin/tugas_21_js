7d681b58c92eedee868d76c1bab575680dec1437
Nurizki <nutrisi1udin@gmail.com>
2022-10-02 20:23:06 +0700

    tugas 21 js
    tugas niomic

---
+++ tugas21.js
@@ -0,0 +1,9 @@
+function kalimat21(){
+    var kal1 = "Saya belajar masak nasi goreng"
+    console.log(kal1)
+    var kata = kal1.replace('masak', 'makan')
+
+    return kata
+}
+
+console.log(kalimat21())
